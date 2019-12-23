3d3023c65063b560f92d00d96f11234ec134e122
Jeff Epler <jepler@gmail.com>
2019-12-23 09:49:55 -0600

    improve build process

--- install.py
+++ install.py
@@ -5,20 +5,38 @@ import subprocess
 from circuitpython_build_tools.build import mpy_cross
 mpy_cross(".venv/bin/mpy-cross", "master")
 
+def match(dest, content):
+    if not os.path.exists(dest): return False
+    with open(dest, "rb") as f: destcontent = f.read()
+    print("# MATCH?", dest, repr(content[:8]), repr(destcontent[:8]), content == destcontent)
+    return content == destcontent
+
 def copy(destdir, srcdir, name, *, destname=None):
     print("# COPY", destdir, name)
     if destname is None: destname = name
-    shutil.copy(os.path.join(srcdir, name), os.path.join(destdir, destname))
+    src = os.path.join(srcdir, name)
+    dest = os.path.join(destdir, destname)
+    with open(src, "rb") as f: content = f.read()
+    put(dest, content)
 
 def mpy_compile(destdir, srcdir, name, *, destname=None):
     print("# CROSS", destdir, name)
     if destname is None: destname = os.path.splitext(name)[0] + ".mpy"
-    subprocess.run(['.venv/bin/mpy-cross', "-o",
-            os.path.join(destdir, destname),
+    desttemp = os.path.join(destdir, destname + ".tmp")
+    dest = os.path.join(destdir, destname)
+    subprocess.run(['.venv/bin/mpy-cross', "-o", desttemp,
             os.path.join(srcdir, name)], check=True)
+    with open(desttemp, "rb") as f: newcontent = f.read()
+    if not match(dest, newcontent):
+        print("# rename", desttemp, dest)
+        os.rename(desttemp, dest)
+    else:
+        print("# unlink", desttemp)
+        os.unlink(desttemp)
 
 def put(dest, content):
-    with open(dest, "w") as f: f.write(content)
+    if not match(dest, content):
+        with open(dest, "wb") as f: f.write(content)
 SRCPATH = 'src'
 DSTPATH = 'CIRCUITPY'
 
@@ -34,4 +52,4 @@ for dirpath, dirnames, filenames in os.walk('src', followlinks=True):
             mpy_compile(outpath, dirpath, f, destname="realmain.mpy")
         elif f.endswith('.py'): mpy_compile(outpath, dirpath, f)
         else: copy(outpath, dirpath, f)
-put("CIRCUITPY/main.py", "import realmain\n")
+put("CIRCUITPY/main.py", b"import realmain\n")
